e163b1d057cbd8cd085058603111d4b37db44dda
Rana_Hamza <descent.hamza153@gmail.com>
2024-10-28 14:45:38 +0500

    updated

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages , setup
+from typing import List
+
+# def get_requirements() -> List[str]:
+    
+#     requirements_list = list[str] = [] # type: ignore
+
+#     return requirements_list
+
+setup(    
+    name='sensor',
+    version='0.1',
+    author="hamza",
+    author_email="descent.hamza153@gmail.com",
+    packages=find_packages(),
+    install_requires= ["pymongo"]    
+)
